9b5a76ced3a892d2c1dc73f1e84e3cec0ec36d81
GeekRes <geekres@126.com>
2019-09-18 14:34:04 +0800

    first commit

---
+++ ethdb/leveldb/db.go
@@ -0,0 +1,205 @@
+package leveldb
+
+import (
+	"fmt"
+
+	"github.com/bmatsuo/lmdb-go/lmdb"
+	"github.com/syndtr/goleveldb/leveldb/iterator"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"github.com/syndtr/goleveldb/leveldb/util"
+)
+
+var MainDB string = "db"
+
+type DB struct {
+	env *lmdb.Env
+	dbi lmdb.DBI
+}
+
+func NewDB(file string) (*DB, error) {
+	env, err := lmdb.NewEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	err = env.SetMaxDBs(1)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(file)
+
+	err = env.Open(file, 0, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	db := &DB{
+		env: env,
+	}
+
+	return db, nil
+}
+
+func (db *DB) GetEnv() *lmdb.Env {
+	return db.env
+}
+
+func (db *DB) Close() error {
+	if db.env != nil {
+		return db.env.Close()
+	}
+
+	return nil
+}
+
+func (db *DB) Get(key []byte, ro *opt.ReadOptions) (value []byte, err error) {
+	txn, err := db.env.BeginTxn(nil, lmdb.Readonly)
+	if err != nil {
+		return nil, err
+	}
+
+	defer txn.Abort()
+	dpi, err := txn.OpenDBI(MainDB, 0)
+
+	if err != nil {
+		return nil, err
+	}
+
+	value, err = txn.Get(dpi, key)
+	db.env.CloseDBI(dpi)
+	return value, err
+}
+
+func (db *DB) Has(key []byte, ro *opt.ReadOptions) (ret bool, err error) {
+	txn, err := db.env.BeginTxn(nil, lmdb.Readonly)
+	if err != nil {
+		return false, err
+	}
+
+	defer txn.Abort()
+	dpi, err := txn.OpenDBI(MainDB, 0)
+
+	if err != nil {
+		return false, err
+	}
+
+	_, err = txn.Get(dpi, key)
+	db.env.CloseDBI(dpi)
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (db *DB) Put(key, value []byte, wo *opt.WriteOptions) error {
+	txn, err := db.env.BeginTxn(nil, 0)
+	if err != nil {
+		return err
+	}
+	defer txn.Abort()
+
+	dpi, err := txn.OpenDBI(MainDB, 0)
+	if err != nil {
+		return err
+	}
+	defer db.env.CloseDBI(dpi)
+
+	err = txn.Put(dpi, key, value, 0)
+	if err != nil {
+		return err
+	}
+	err = txn.Commit()
+	return err
+}
+
+func (db *DB) Delete(key []byte, wo *opt.WriteOptions) error {
+	txn, err := db.env.BeginTxn(nil, 0)
+	if err != nil {
+		return err
+	}
+	defer txn.Abort()
+
+	dpi, err := txn.OpenDBI(MainDB, 0)
+	if err != nil {
+		return err
+	}
+	defer db.env.CloseDBI(dpi)
+
+	err = txn.Del(dpi, key, nil)
+	if err != nil {
+		return err
+	}
+	err = txn.Commit()
+	return err
+}
+
+func (db *DB) NewIterator(slice *util.Range, ro *opt.ReadOptions) iterator.Iterator {
+	return nil
+}
+
+func (db *DB) GetProperty(name string) (value string, err error) {
+	return "", nil
+}
+
+func (db *DB) CompactRange(r util.Range) error {
+	return nil
+}
+
+func (db *DB) Write(batch *Batch, wo *opt.WriteOptions) error {
+	return batch.txn.Commit()
+}
+
+type BatchReplay interface {
+	Put(key, value []byte)
+	Delete(key []byte)
+}
+
+type Batch struct {
+	txn      *lmdb.Txn
+	dbi      lmdb.DBI
+	errornum int
+}
+
+func NewBatch(env *lmdb.Env) *Batch {
+	txn, err := env.BeginTxn(nil, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	dbi, err := txn.OpenDBI(MainDB, lmdb.Create)
+	if err != nil {
+		panic(err)
+	}
+
+	b := &Batch{
+		txn: txn,
+		dbi: dbi,
+	}
+
+	return b
+}
+
+func (b *Batch) Put(key, value []byte) {
+	err := b.txn.Put(b.dbi, key, value, 0)
+
+	if err != nil {
+		b.errornum++
+	}
+}
+
+func (b *Batch) Delete(key []byte) {
+	err := b.txn.Del(b.dbi, key, nil)
+	if err != nil {
+		b.errornum++
+	}
+}
+func (b *Batch) Reset() {
+	b.txn.Reset()
+}
+
+func (b *Batch) Replay(r BatchReplay) error {
+	return nil
+}
---
+++ ethdb/leveldb/lmdb.go
@@ -0,0 +1,218 @@
+package leveldb
+
+import (
+	"sync"
+	"time"
+
+	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/syndtr/goleveldb/leveldb/util"
+)
+
+const (
+	// degradationWarnInterval specifies how often warning should be printed if the
+	// leveldb database cannot keep up with requested writes.
+	degradationWarnInterval = time.Minute
+
+	// minCache is the minimum amount of memory in megabytes to allocate to leveldb
+	// read and write caching, split half and half.
+	minCache = 16
+
+	// minHandles is the minimum number of files handles to allocate to the open
+	// database files.
+	minHandles = 16
+
+	// metricsGatheringInterval specifies the interval to retrieve leveldb database
+	// compaction, io and pause stats to report to the user.
+	metricsGatheringInterval = 3 * time.Second
+)
+
+// Database is a persistent key-value store. Apart from basic data storage
+// functionality it also supports batch writes and iterating over the keyspace in
+// binary-alphabetical order.
+type Database struct {
+	fn string // filename for reporting
+	db *DB    // LevelDB instance
+
+	quitLock sync.Mutex      // Mutex protecting the quit channel access
+	quitChan chan chan error // Quit channel to stop the metrics collection before closing the database
+
+	log log.Logger // Contextual logger tracking the database path
+}
+
+// New returns a wrapped LevelDB object. The namespace is the prefix that the
+// metrics reporting should use for surfacing internal stats.
+func New(file string, cache int, handles int, namespace string) (*Database, error) {
+	db, err := NewDB(file)
+	if err != nil {
+		return nil, err
+	}
+
+	logger := log.New("database", file)
+
+	database := &Database{
+		fn:       file,
+		db:       db,
+		log:      logger,
+		quitChan: make(chan chan error),
+	}
+
+	return database, nil
+}
+
+// Close stops the metrics collection, flushes any pending data to disk and closes
+// all io accesses to the underlying key-value store.
+func (db *Database) Close() error {
+	db.quitLock.Lock()
+	defer db.quitLock.Unlock()
+
+	if db.quitChan != nil {
+		errc := make(chan error)
+		db.quitChan <- errc
+		if err := <-errc; err != nil {
+			db.log.Error("Metrics collection failed", "err", err)
+		}
+		db.quitChan = nil
+	}
+	return db.db.Close()
+}
+
+// Has retrieves if a key is present in the key-value store.
+func (db *Database) Has(key []byte) (bool, error) {
+	return db.db.Has(key, nil)
+}
+
+// Get retrieves the given key if it's present in the key-value store.
+func (db *Database) Get(key []byte) ([]byte, error) {
+	dat, err := db.db.Get(key, nil)
+	if err != nil {
+		return nil, err
+	}
+	return dat, nil
+}
+
+// Put inserts the given value into the key-value store.
+func (db *Database) Put(key []byte, value []byte) error {
+	return db.db.Put(key, value, nil)
+}
+
+// Delete removes the key from the key-value store.
+func (db *Database) Delete(key []byte) error {
+	return db.db.Delete(key, nil)
+}
+
+// NewBatch creates a write-only key-value store that buffers changes to its host
+// database until a final write is called.
+func (db *Database) NewBatch() ethdb.Batch {
+	return &batch{
+		db: db.db,
+		b:  NewBatch(db.db.GetEnv()),
+	}
+}
+
+// NewIterator creates a binary-alphabetical iterator over the entire keyspace
+// contained within the leveldb database.
+func (db *Database) NewIterator() ethdb.Iterator {
+	return db.db.NewIterator(new(util.Range), nil)
+}
+
+// NewIteratorWithStart creates a binary-alphabetical iterator over a subset of
+// database content starting at a particular initial key (or after, if it does
+// not exist).
+func (db *Database) NewIteratorWithStart(start []byte) ethdb.Iterator {
+	return db.db.NewIterator(&util.Range{Start: start}, nil)
+}
+
+// NewIteratorWithPrefix creates a binary-alphabetical iterator over a subset
+// of database content with a particular key prefix.
+func (db *Database) NewIteratorWithPrefix(prefix []byte) ethdb.Iterator {
+	return db.db.NewIterator(util.BytesPrefix(prefix), nil)
+}
+
+// Stat returns a particular internal stat of the database.
+func (db *Database) Stat(property string) (string, error) {
+	return db.db.GetProperty(property)
+}
+
+// Compact flattens the underlying data store for the given key range. In essence,
+// deleted and overwritten versions are discarded, and the data is rearranged to
+// reduce the cost of operations needed to access them.
+//
+// A nil start is treated as a key before all keys in the data store; a nil limit
+// is treated as a key after all keys in the data store. If both is nil then it
+// will compact entire data store.
+func (db *Database) Compact(start []byte, limit []byte) error {
+	return db.db.CompactRange(util.Range{Start: start, Limit: limit})
+}
+
+// Path returns the path to the database directory.
+func (db *Database) Path() string {
+	return db.fn
+}
+
+// batch is a write-only leveldb batch that commits changes to its host database
+// when Write is called. A batch cannot be used concurrently.
+type batch struct {
+	db   *DB
+	b    *Batch
+	size int
+}
+
+// Put inserts the given value into the batch for later committing.
+func (b *batch) Put(key, value []byte) error {
+	b.b.Put(key, value)
+	b.size += len(value)
+	return nil
+}
+
+// Delete inserts the a key removal into the batch for later committing.
+func (b *batch) Delete(key []byte) error {
+	b.b.Delete(key)
+	b.size++
+	return nil
+}
+
+// ValueSize retrieves the amount of data queued up for writing.
+func (b *batch) ValueSize() int {
+	return b.size
+}
+
+// Write flushes any accumulated data to disk.
+func (b *batch) Write() error {
+	return b.db.Write(b.b, nil)
+}
+
+// Reset resets the batch for reuse.
+func (b *batch) Reset() {
+	b.b.Reset()
+	b.size = 0
+}
+
+// Replay replays the batch contents.
+func (b *batch) Replay(w ethdb.KeyValueWriter) error {
+	return b.b.Replay(&replayer{writer: w})
+}
+
+// replayer is a small wrapper to implement the correct replay methods.
+type replayer struct {
+	writer  ethdb.KeyValueWriter
+	failure error
+}
+
+// Put inserts the given value into the key-value data store.
+func (r *replayer) Put(key, value []byte) {
+	// If the replay already failed, stop executing ops
+	if r.failure != nil {
+		return
+	}
+	r.failure = r.writer.Put(key, value)
+}
+
+// Delete removes the key from the key-value data store.
+func (r *replayer) Delete(key []byte) {
+	// If the replay already failed, stop executing ops
+	if r.failure != nil {
+		return
+	}
+	r.failure = r.writer.Delete(key)
+}
